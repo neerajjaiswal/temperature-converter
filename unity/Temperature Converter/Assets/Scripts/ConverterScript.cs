b8e86102d7e943ee3f54eea66cefdba0414e084d
Neeraj Jaiswal <neeraj@appnimi.com>
2018-06-18 13:27:35 +0530

    First Commit

---
+++ unity/Temperature Converter/Assets/Scripts/ConverterScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConverterScript : MonoBehaviour {
+
+	public Text inputTemperature;
+	public Text outputTemperature;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void handleCelsiusButtonClick(){
+		Debug.Log ("handleCelsiusButtonClick");
+
+		float fahrenheit = float.Parse(inputTemperature.text);
+
+		float celsius = ((fahrenheit - 32)*5)/9;
+
+		outputTemperature.text = celsius.ToString()+" Celsius";
+
+	}
+
+	public void handleFahrenheitButtonClick(){
+		Debug.Log ("handleFahrenheitButtonClick");
+
+		float celsius = float.Parse(inputTemperature.text);
+
+		float fahrenheit = (celsius * 9 / 5) + 32;
+
+		outputTemperature.text = fahrenheit.ToString()+" Fahrenheit";
+	}
+}
